ab9bb86969c79b6c52c18c3dca4e3a7779c5bd6a
Shefa <zeyna@example.com>
2025-04-29 15:20:33 +0400

    added task4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //int[] arr = { 2, 3, 4, 5, };
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum += arr[i];
+            //}
+            //Console.WriteLine(sum);
+            //task2
+            //int[] arr = { 12, 14, 90, 45 };
+            //int temp = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > temp)
+            //    {
+            //        temp = arr[i];
+            //    }
+            //}
+            //Console.WriteLine(temp);
+            //task3
+            //int[] arr = { 12, 35, 2, 5, 10, 11 };
+            //int sum= arr[0]+arr[arr.Length-1];
+            //Console.WriteLine(sum);
+            //task4
+            //int n = 32;
+            //while (n > 1)
+            //{
+            //    if (n % 2 != 0)
+            //    {
+            //        Console.WriteLine("2nin quvveti deyil");
+            //        return;
+            //    }
+            //    n /= 2;
+            //}
+            //Console.WriteLine("2-nin quvvetidir");
+            //task5
+            //int n = 234527;
+            //int say = 0;
+            //while (n > 0) {
+            //    say++;
+            //    n/= 10;
+            //}
+            //Console.WriteLine(say);
+            //task6
+            //int[] num = { 10, 20, 30, 43, 50, 79 };
+            //int x = 50;
+            //int index = -1;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    if (num[i] == x)
+            //    {
+            //       index = i;    
+            //       break;
+            //    }
+            //}
+            //Console.WriteLine(index);
+            //task7
+            //int[] num = { 114, 12, 131, 14, 1, 23, 3 };
+            //int sum = 0;  
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    int n = num[i];
+            //    int say = 0;
+            //    while (n > 0)
+            //    {
+            //        say++;
+            //        n /= 10;
+            //    }
+            //    if (say > 2)
+            //    {
+            //        sum += num[i];
+            //    }
+            //}
+            //Console.WriteLine(sum);
+            //task8
+
+
+        }
+
+
+    }
+}
+
+
